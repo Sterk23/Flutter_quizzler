4e27d5bf6fc255ebf1c4c478d3f02f94b71bf5d6
Thananon <thananon@digio.co.th>
2020-12-03 14:53:52 +0700

    Try to solve the code by using only List

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questionList = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answer = [false, true, true];
+  int questionNumber =0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionList[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +70,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+              updateScore(true);
+
+
               },
             ),
           ),
@@ -80,15 +91,39 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                updateScore(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children:scoreKeeper
+        ),
       ],
     );
   }
-}
+
+  void updateScore(bool ans) {
+    setState(() {
+      if(ans == answer[questionNumber]) {
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      }else{
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+      if(questionNumber<3)
+      questionNumber++;
+      // questionList[questionNumber];
+    });
+
+    }
+  }
+
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
